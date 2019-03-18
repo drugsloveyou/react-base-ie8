2e84547295dac34d3c1b554b0490eb22d5a26625
2fps <zyt1015442526@163.com>
2019-03-18 12:42:18 +0800

    增加react-router路由分包

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,16 @@ import { createHashHistory } from "history";
 const history = useRouterHistory(createHashHistory)({ queryKey: false });
 
 import About from "./About";
-import Inbox from "./Inbox";
+// import Inbox from "./Inbox";
+
+// react-router 按路由打包（分包）
+// 生产环境，用的 webpack-parallel-uglify-plugin 打包的，由于它是采用 commonJS规范的，所以require不需要default，
+// 开发环境下运行于浏览器，用的是es6规范，并且组件都是通过 export default 导出的，所有需要加入 .default
+const Inbox  = (location, cb) => {
+  require.ensure([], require => {
+    cb(null, require('./Inbox').default || require('./Inbox'))
+  }, 'Inbox');
+};
 
 let msg = `Fetching data.json`;
 class App extends Component {
@@ -38,7 +47,7 @@ class App extends Component {
         <Router history={history}>
           <Route path="/" component={Test}>
             <Route path="about" component={About} />
-            <Route path="inbox" component={Inbox} />
+            <Route path="inbox" getComponent={Inbox} />
           </Route>
         </Router>
       </div>
